c543c5fb588ecb7bd108f17e7e6c5679b94986ef
Q4NSIQ@stud.uni-obuda.hu <Q4NSIQ@stud.uni-obuda.hu>
2021-10-03 21:22:52 +0200

    Microsoft.EntityFrameworkCore .Proxies .Tools .SqlServer installed and MoviesDB added and set under .Data

--- Q4NSIQ_HFT_2021221/Q4NSIQ_HFT_2021221.Client/Program.cs
+++ Q4NSIQ_HFT_2021221/Q4NSIQ_HFT_2021221.Client/Program.cs
@@ -6,7 +6,7 @@ namespace Q4NSIQ_HFT_2021221.Client
     {
         static void Main(string[] args)
         {
-            //
+            //Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\MoviesDB.mdf;Integrated Security=True
         }
     }
 }
