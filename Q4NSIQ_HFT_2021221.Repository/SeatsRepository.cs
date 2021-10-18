9a20ad414dc050423b525783ca00eefa6a37be58
Q4NSIQ@stud.uni-obuda.hu <Q4NSIQ@stud.uni-obuda.hu>
2021-10-18 15:32:34 +0200

    Repository classes got constructors

--- Q4NSIQ_HFT_2021221.Repository/SeatsRepository.cs
+++ Q4NSIQ_HFT_2021221.Repository/SeatsRepository.cs
@@ -3,10 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Q4NSIQ_HFT_2021221.Data;
+using Q4NSIQ_HFT_2021221.Models;
 
 namespace Q4NSIQ_HFT_2021221.Repository
 {
     class SeatsRepository
     {
+        CinemaDbContext db;
+
+        public SeatsRepository(CinemaDbContext db)
+        {
+            this.db = db;
+        }
     }
 }
--- Q4NSIQ_HFT_2021221.Repository/TicketRepository.cs
+++ Q4NSIQ_HFT_2021221.Repository/TicketRepository.cs
@@ -3,10 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Q4NSIQ_HFT_2021221.Data;
+using Q4NSIQ_HFT_2021221.Models;
 
 namespace Q4NSIQ_HFT_2021221.Repository
 {
     class TicketRepository
     {
+        CinemaDbContext db;
+
+        public TicketRepository(CinemaDbContext db)
+        {
+            this.db = db;
+        }
     }
 }
