cc3dedd9a645578f1e32539089160bb8ad1a141e
Q4NSIQ@stud.uni-obuda.hu <Q4NSIQ@stud.uni-obuda.hu>
2021-12-02 21:02:15 +0100

    New Postman Invite link has been inserted

--- Q4NSIQ_HFT_2021221.Client/Program.cs
+++ Q4NSIQ_HFT_2021221.Client/Program.cs
@@ -6,12 +6,27 @@ namespace Q4NSIQ_HFT_2021221.Client
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello!");
-            Console.WriteLine(@"POSTMAN invite link: https://app.getpostman.com/join-team?invite_code=0279391ad6191a2a9dd581246b082782");
+            Console.WriteLine("POSTMAN invite link:");
+            Console.WriteLine(@"https://app.getpostman.com/join-team?invite_code=2f276736ee8cf9143cfdbc071aaa1185&ws=fd1fa18d-e16e-4bb0-8387-0acc48a7c7f7");
+            Console.WriteLine
+            (@"
+                                      ___________I____________
+                                     ( _____________________ ()
+                                   _.-'|                    ||
+                               _.-'   ||       WELCOME      ||
+              ______       _.-'       ||                    ||
+             |      |_ _.-'           ||   CINEMA DATABASE  ||
+             |      |_|_              ||                    ||
+             |______|   `-._          ||         IS         ||
+                /\          `-._      ||                    ||
+               /  \             `-._  ||       LOADING      ||
+              /    \                `-.|____________________||
+             /      \                 ------------------------
+            /________\___________________/________________\______"
+            );
             System.Threading.Thread.Sleep(8000);
             
-            IRestService restService = new RestService(@"http://localhost:17133");
-            MenuTasks menuHelper = new MenuTasks(restService);
+            MenuTasks menuHelper = new MenuTasks(new RestService(@"http://localhost:17133"));
             menuHelper.Start();
         }
     }
--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # Q4NSIQ_HFT_2021221
-POSTMAN invite link: https://app.getpostman.com/join-team?invite_code=0279391ad6191a2a9dd581246b082782
+POSTMAN invite link: https://app.getpostman.com/join-team?invite_code=2f276736ee8cf9143cfdbc071aaa1185&ws=fd1fa18d-e16e-4bb0-8387-0acc48a7c7f7
